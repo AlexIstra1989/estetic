ccc25f3e18d0f3b1a12a2c0ee80fc287fd332e93
AlexIstra1989 <alex888fcsm@gmail.com>
2022-12-01 23:03:28 +0300

    added masters block

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -103,6 +103,72 @@ function initSliders() {
 			}
 		});
 	}
+ if (document.querySelector('.masters-slider__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.masters-slider__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Autoplay, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 2,
+			spaceBetween: 10,
+			autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+
+			// Пагинация
+			
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// Брейкпоинты
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+			},
+			// События
+			on: {
+
+			}
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
